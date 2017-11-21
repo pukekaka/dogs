755ea5625bbbab6a6a773516f4cac3489982cf92
pukekakaster <pukekakaster@gmail.com>
2017-11-21 09:16:50 +0900

    opcode_opnd

--- change_datafile.py
+++ change_datafile.py
@@ -1,75 +1,65 @@
 import json
-from collections import OrderedDict
-#
-# data = {}
-# event = {}
-#
-# event['type'] = 1
-# event['value'] = 3
-#
-# for i in range(1, 10):
-#     data[i] = event
-#
-# print json.dumps(data, sort_keys=True, indent=4)
-
 import os
 
-data_directory = 'E:/Works/Data/samples/malwares/APT/'
+data_directory = 'E:/Works/Data/samples/output/'
+output_directory = 'E:/Works/Data/samples/output_c/'
+output_filename = 'opcode_opnd'
 
-asmf_extension = 'asmf'
-json_extension = 'json'
+bb_extension = 'json'
 bblist_extension = 'bblist'
-filelist = []
-asmf_filelist = []
-bblist_filelist = []
-json_filelit = []
+bb_filenamelist = []
+bblist_filenamelist = []
 
-filecount = 0
-asmf_count = 0
-bblist_count = 0
-json_count = 0
+print('=> filename read start')
 
 for root, dirs, files in os.walk(data_directory):
     for file in files:
 
-        filelist.append(file)
-        filecount += 1
+        filename_split = file.split('.')
+        extension_loc = len(filename_split) - 1
+        if bb_extension == filename_split[extension_loc]:
+            bb_filenamelist.append(root+file)
 
-        filename = file.split('.')
-        extension_loc = len(filename) - 1
-        if asmf_extension == filename[extension_loc] :
-            asmf_filelist.append(file)
-            asmf_count += 1
+        elif bblist_extension == filename_split[extension_loc]:
+            bblist_filenamelist.append(root+file)
 
-        if bblist_extension == filename[extension_loc] :
-            bblist_filelist.append(file)
-            bblist_count += 1
+print('=> filename read end')
 
-print ('file count :', filecount)
-print ('asmf count :', asmf_count)
-print ('bblist count :', bblist_count)
+bb_data_list = []
+bblist_data_list = []
+count = 1
 
-print ('filelist count', len(filelist))
-print ('asmf filelist count', len(asmf_filelist))
-print ('bblist filelist count', len(bblist_filelist))
+print('=> json object read start')
 
-print(asmf_filelist)
+for bb_filename in bb_filenamelist:
+    bb_data_file = open(bb_filename, encoding="utf-8")
+    bb_data = json.load(bb_data_file)
+    bb_data_list.append(bb_data)
+    print(count, 'complete', bb_filename)
+    count += 1
 
-binary = []
-bbl = []
-bbl_list = []
-func = []
-func_list = []
+print('=> json object read end')
+print('=> opcode + opnd make start')
 
+word_list = []
+bbcount = 1
+for bb_data in bb_data_list:
+    for inst in bb_data['insts']:
+        opcode = inst['opcode']
+        opnd1 = inst['opnd1']
+        opnd2 = inst['opnd2']
+        opnd3 = inst['opnd3']
+        word = opcode.strip()+opnd1.strip()+opnd2.strip()+opnd3.strip()
+        word = word.strip()
+        word_list.append(word)
+    print(bbcount, 'complete', bb_data['filename'])
+    bbcount += 1
 
+print('=> opcode + opnd make end')
+print('=> opcode + opnd write start')
 
+output_file = open(output_directory + output_filename, 'w')
+output_file.write(" ".join(word_list))
+output_file.close()
 
-
-
-# group_data = OrderedDict()
-# albums = OrderedDict()
-#
-# group_data["binary"] = 'xxxx'
-# group_data["flist"] = ['sowon', "yerin", "eunha", "youju"]
-#
-# print(json.dumps(group_data, ensure_ascii=False, indent=4))
+print('=> opcode + opnd write end')
