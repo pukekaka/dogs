cc48c71eec8c0b58d3458483d4e5d47771f95a02
pukekaka <pukekakaster@gmail.com>
2017-12-04 14:18:49 +0900

    save & commit

---
+++ 05_bin2vec.py
@@ -0,0 +1,22 @@
+from gensim.models import doc2vec
+from gensim.models.doc2vec import TaggedDocument
+import os
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+filename = 'basicblock_by_file'
+model_directory = 'model'
+modelname = filename+'_doc2vec.model'
+modellistname = 'model_list'
+model_path = os.path.join(current_directory, model_directory, modelname)
+model_list_path = os.path.join(current_directory, model_directory, modellistname)
+
+
+model = doc2vec.Doc2Vec.load(model_path)
+funcvec = model.docvecs['fff564d59deec80ad5fcc92867e07b69_17']
+# sims = model.docvecs.most_similar('02456317d10e2d769f704935d5b5b6cd_2137')
+# sims2 = model.most_similar('intthree')
+print(funcvec)
+# print(sims2)
